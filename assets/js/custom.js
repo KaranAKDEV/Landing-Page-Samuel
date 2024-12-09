056f7b7e823888be86edda7466f7b9d6fd5c5100
KaranAKDEV <109277189+KaranAKDEV@users.noreply.github.com>
2024-12-09 21:13:58 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+	"use strict";
+
+    // Scroll To Top 
+		$(window).on('scroll',function () {
+			if ($(this).scrollTop() > 600) {
+				$('.return-to-top').fadeIn();
+			} else {
+				$('.return-to-top').fadeOut();
+			}
+		});
+		$('.return-to-top').on('click',function(){
+				$('html, body').animate({
+				scrollTop: 0
+			}, 1500);
+			return false;
+		});
+	
+	// slick carousel
+	    $(".testimonial-carousel").slick({
+	        infinite: true,
+	        centerMode: true,
+	        autoplay:true,
+	        slidesToShow: 5,
+	        slidesToScroll: 3,
+	        autoplaySpeed:1500,
+			responsive: [
+				{
+
+					breakpoint:1440,
+					settings: {
+					slidesToShow:3
+					}
+
+				},
+				{
+
+					breakpoint: 1024,
+					settings: {
+					slidesToShow:2,
+					
+					}
+
+				}, 
+				{
+
+					breakpoint:991,
+					settings: {
+					slidesToShow:2,
+					centerMode:false,
+					}
+
+				},
+				{
+
+					breakpoint:767,
+					settings: {
+					slidesToShow:1,
+					}
+
+				}
+			]
+	    });
+
+		feather.replace();
+
+	// counter
+		$(window).on('load', function(){	
+			$('.counter').counterUp({
+				delay: 10,
+				time: 3000
+			});	
+		});
+
+});
